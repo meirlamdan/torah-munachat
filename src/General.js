4a3e9b66efa8f04e0524e9fc829128b5cdf487e5
meir lamdan <mchlerner@gmail.com>
2021-08-09 15:09:29 +0300

    img

--- src/General.js
+++ src/General.js
@@ -1,7 +1,7 @@
 export const General = [
     {
         name: 'בייס הספר' ,
-        src: require('/src/assets/basehasefer.png'),
+        src: require('@/assets/basehasefer.png'),
         about: 'חיפוש משוכלל בתנ"ך',
         href: 'http://www.basehasefer.com/'
     },
