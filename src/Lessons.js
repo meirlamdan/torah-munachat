16be1dcb037141394b7fd7e6add7ffff373535e5
meir lamdan <mchlerner@gmail.com>
2021-10-04 10:48:54 +0300

    more sites

--- src/Lessons.js
+++ src/Lessons.js
@@ -1,18 +1,21 @@
+import { Promise } from "core-js"
+import { reject, resolve } from "core-js/fn/promise"
+
 export const lessons = [
     {
-        name: 'tora hany time' ,
+        name: 'tora hany time',
         src: 'https://www.torahanytime.com/static/images/logo.png',
         about: 'כיום, TorahAnytime היא הספרייה הגדולה ביותר של קטעי וידאו תורניים שהוקלטו במקור בעולם , עם 70,000+ סרטוני תורה מיותר מ -1,000 דוברים שונים.',
         href: 'https://www.torahanytime.com/'
     },
     {
-        name: 'קול הלשון' ,
+        name: 'קול הלשון',
         src: 'https://www.kolhalashon.com/imgs/logo_round.png',
         about: 'שעורים רבים בכל מקצעות התורה ',
         href: 'https://www.kolhalashon.com/'
     },
     {
-        name: 'דירשו' ,
+        name: 'דירשו',
         src: 'https://www.dirshu.co.il/wp-content/uploads/2021/01/%D7%93%D7%A8%D7%A9%D7%95@2x-8.png',
         about: 'שיעורים בדף היומי ובהלכה ומועדים וזמנים ',
         href: 'https://www.dirshu.co.il/'
@@ -26,3 +29,4 @@ export const lessons = [
 
 
 ]
+
