7095ab85e509856f42095b5a19c8602b0aaa4e3b
meir lerner <mchlerner@gmail.com>
2021-11-18 14:38:38 +0200

    fix

--- src/General.js
+++ src/General.js
@@ -1,50 +1,50 @@
 export const General = [
-    {
-        name: 'בייס הספר',
-        src: require('@/assets/basehasefer.png'),
-        about: 'חיפוש משוכלל בתנ"ך',
-        href: 'http://www.basehasefer.com/'
-    },
-    {
-        name: 'FJMS - אגודת פרידברג לכתבי יד יהודיים',
-        src: 'https://fjms.genizah.org/ImagesFJMS/logo.png',
-        about: 'הכי גרסינן- להשוואת שינויי הנוסחאות שבש"ס. גניזת קהיר. השוואת נוסחאות הרמב"ם. ועוד  ',
-        href: 'https://fjms.genizah.org/'
-    },
-    {
-        name: 'אספקלריא',
-        about: 'קובץ אנציקלופדי למחשבת היהדות',
-        href: 'http://www.aspaklaria.info/'
-    },
-    {
-        name: 'פורום לתורה',
-        src: 'https://images.matara.pro/ClientsImages/9001941.jpg?',
-        about: 'פורום תורני בכל מקצועות התורה ',
-        href: 'https://tora-forum.co.il/'
-    },
-    {
-        name: 'ויקי סוגיה',
-        src: 'https://d1qvck26m1aukd.cloudfront.net/app/public/media_objects/image_4x3_hes/000/003/187/big/1fe7c68fd4edb16529323490066f651f31a80ece.png?1552837203',
-        about: 'יצירה תורנית שיתופית',
-        href: 'https://www.betmidrash.org.il/'
-    },
-    {
-        name: 'פורטל הדף היומי',
-        src: 'https://daf-yomi.com/images/logo.gif',
-        about: 'פירושים ושיעורים ועזרים רבים ללומדי דף היומי',
-        href: 'https://daf-yomi.com/'
-    },
-    {
-        name: 'נתיבות הש"ס',
-        about: 'נושאים ומקבילות בתלמוד הבבלי',
-        href: 'http://www.netivothashas.org.il/'
-    },
-    {
-        name: 'עלונים לשבת להורדה',
-        src: 'https://yiddishevinkel.com/wp-content/uploads/2020/02/yiddsh_logo-01.png',
-        about: 'מבחר עלונים לשבת להורדה',
-        href: 'https://yiddishevinkel.com/gilyones/'
-    }
-
+  {
+    name: 'בייס הספר',
+    src: require('@/assets/basehasefer.png'),
+    about: 'חיפוש משוכלל בתנ"ך',
+    href: 'http://www.basehasefer.com/'
+  },
+  {
+    name: 'FJMS - אגודת פרידברג לכתבי יד יהודיים',
+    src: 'https://fjms.genizah.org/ImagesFJMS/logo.png',
+    about: 'הכי גרסינן- להשוואת שינויי הנוסחאות שבש"ס. גניזת קהיר. השוואת נוסחאות הרמב"ם. ועוד  ',
+    href: 'https://fjms.genizah.org/'
+  },
+  {
+    name: 'אספקלריא',
+    about: 'קובץ אנציקלופדי למחשבת היהדות',
+    href: 'http://www.aspaklaria.info/'
+  },
+  {
+    name: 'פורום לתורה',
+    src: 'https://images.matara.pro/ClientsImages/9001941.jpg?',
+    about: 'פורום תורני בכל מקצועות התורה ',
+    href: 'https://tora-forum.co.il/'
+  },
+  {
+    name: 'ויקי סוגיה',
+    src: 'https://d1qvck26m1aukd.cloudfront.net/app/public/media_objects/image_4x3_hes/000/003/187/big/1fe7c68fd4edb16529323490066f651f31a80ece.png?1552837203',
+    about: 'יצירה תורנית שיתופית',
+    href: 'https://www.betmidrash.org.il/'
+  },
+  {
+    name: 'פורטל הדף היומי',
+    src: 'https://daf-yomi.com/images/logo.gif',
+    about: 'פירושים ושיעורים ועזרים רבים ללומדי דף היומי',
+    href: 'https://daf-yomi.com/'
+  },
+  {
+    name: 'נתיבות הש"ס',
+    about: 'נושאים ומקבילות בתלמוד הבבלי',
+    href: 'http://www.netivothashas.org.il/'
+  },
+  {
+    name: 'עלונים לשבת להורדה',
+    src: 'https://yiddishevinkel.com/wp-content/uploads/2020/02/yiddsh_logo-01.png',
+    about: 'מבחר עלונים לשבת להורדה',
+    href: 'https://yiddishevinkel.com/gilyones/'
+  }
 
 ]
+;
