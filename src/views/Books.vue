5bbfdacaad98c4a103f206f124af5ddb22edb8f8
meir lerner <mchlerner@gmail.com>
2024-05-07 01:12:33 +0300

    fix

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,12 +1,12 @@
   <script setup>
   import books from "@/data/books";
-  import AboutCategory from "../components/Category.vue";
+  import Category from "@/components/Category.vue";
   import Cards from "@/components/Cards.vue";
 </script>
 
 <template>
   <div>
-    <about-category icon="tabler:books" title="מאגרי ספרים" about="כאן מרוכזים מבחר מאגרי ספרים " />
+    <Category icon="tabler:books" title="מאגרי ספרים" about="כאן מרוכזים מבחר מאגרי ספרים " />
     <Cards :sites="books" />
   </div>
 </template>
--- src/views/Lessons.vue
+++ src/views/Lessons.vue
@@ -1,15 +1,15 @@
 <script setup>
-import AboutCategory from "../components/Category.vue";
-import CardS from "../components/Cards.vue";
+import Category from "../components/Category.vue";
+import Cards from "../components/Cards.vue";
 import lessons from "@/data/lessons";
 </script>
 
 
 <template>
   <div>
-    <about-category icon="material-symbols:adaptive-audio-mic-rounded" title="שיעורים  ודרשות"
+    <Category icon="material-symbols:adaptive-audio-mic-rounded" title="שיעורים  ודרשות"
       about="כאן מרוכזים אתרים עם שיעורים והרצאות בכל מקצועות התורה " />
-    <CardS :sites="lessons" />
+    <Cards :sites="lessons" />
   </div>
 </template>
 
