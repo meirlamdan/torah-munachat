ba2004a3af8f42f6ddedc85578d09f202729f459
meir lerner <mchlerner@gmail.com>
2024-05-20 18:17:02 +0300

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
+
 import Books from '@/views/Books.vue';
 import Lessons from '@/views/Lessons.vue';
 import General from '@/views/General.vue';
 import NotFound from '@/views/NotFound.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'Books',
+    component: Books
+  },
+  {
+    path: '/lessons',
+    name: 'Lessons',
+    component: Lessons
+  },
+  {
+    path: '/general',
+    name: 'General',
+    component: General
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFound
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Books',
-      component: Books
-    },
-    {
-      path: '/lessons',
-      name: 'Lessons',
-      component: Lessons
-    },
-    {
-      path: '/general',
-      name: 'General',
-      component: General
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      component: NotFound
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/About.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
